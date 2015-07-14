d50a39bcb10e6d3094c95a3ea5a83ab54b8fbcf0
shawn anderson <shawn_anderson@gov.nt.ca>
2015-07-14 15:49:16 -0600

    finished coin sum using recursion

--- 31_coinSums.scala
+++ 31_coinSums.scala
@@ -14,7 +14,7 @@ How many different ways can £2 be made using any number of coins?
 import scala.collection.mutable.ListBuffer
 
 val mem = new ListBuffer[Int]
-
+/*
 for(i <- (0 to 200))yield { i match {
 	case n if (n >= 200) => mem.insert(n, mem(n - 200) + mem(n - 100) + mem(n - 50) + mem(n - 20) + mem(n - 10) + mem(n - 5) + mem(n - 2) + mem(n - 1))
 	case n if (n >= 100) => mem.insert(n, mem(n - 100) + mem(n - 50) + mem(n - 20) + mem(n - 10) + mem(n - 5) + mem(n - 2) + mem(n - 1))
@@ -25,4 +25,15 @@ for(i <- (0 to 200))yield { i match {
 	case n if (n >= 2)   => mem.insert(n, mem(n - 2) + mem(n - 1))
 	case n if (n > 1)    => mem.insert(n, mem(n - 1))
 	case n if (n <= 1)   => mem.insert(n, 1)
-}}
+}} */
+
+val S = List(1, 2, 5, 10, 20, 50, 100, 200).reverse
+val m = 8
+val n = 200
+
+def calc(S: List[Int], m: Int, n: Int): Int = {
+	if (n == 0) return 1
+	if (n < 0)  return 0
+	if (m <= 0) return 0
+	return calc(S, m-1, n) + calc(S, m, n - S(m-1))
+}
